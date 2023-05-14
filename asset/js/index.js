34acdcf9af276eb9fb9dfd0a1337cfe74ed27c62
Thandavamoorthy <thandavamoorthyk02@gmail.com>
2023-05-14 23:29:56 +0530

    second commit

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,6 +1,13 @@
-const toggleBtn = document.querySelector(".toggle-btn"); 
-const menu = document.querySelector(".menu"); 
+const toggleBtn = document.querySelector(".toggle-btn");
+const menu = document.querySelector(".menu");
+const navLink = document.querySelectorAll(".nav-links");
+const body = document.getElementsByTagName("body");
 
-toggleBtn.addEventListener("click", function(){
+toggleBtn.addEventListener("click", function () {
     menu.classList.toggle("toggle");
 })
+
+navLink.forEach(e => e.addEventListener("click", function () {
+    menu.classList.remove("toggle");
+}))
+
